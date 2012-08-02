c1b2e08dff0bb4cf10c66f3ada43afc22f41b34e
jurav8 <jurav8@gmail.com>
2012-08-02 18:26:08 -0300

    main

--- src/org/proyectofinal/gestorpacientes/Sistema.java
+++ src/org/proyectofinal/gestorpacientes/Sistema.java
@@ -1,16 +1,14 @@
 package org.proyectofinal.gestorpacientes;
 
 import java.text.ParseException;
+
 import javax.swing.UIManager;
 import javax.swing.UnsupportedLookAndFeelException;
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-import org.proyectofinal.gestorpacientes.modelo.ModeloMedico;
-import org.proyectofinal.gestorpacientes.modelo.ModeloUsuario;
+import org.proyectofinal.gestorpacientes.modelo.Manejador;
 import org.proyectofinal.gestorpacientes.modelo.entidades.Login;
-import org.proyectofinal.gestorpacientes.modelo.entidades.Medico;
-import org.proyectofinal.gestorpacientes.modelo.entidades.Usuario;
 import org.proyectofinal.gestorpacientes.vista.Administrador;
 import org.proyectofinal.gestorpacientes.vista.Splash;
 
@@ -57,6 +55,8 @@ public class Sistema {
 		sp = new Splash();
 		sp.getBarra().addChangeListener(new ChangeListener() {
 			public void stateChanged(ChangeEvent arg0) {
+				
+				Manejador.getInstancia(false, false);
 
 				if (sp.getBarra().getValue() == 100) {
 					sp.dispose();
@@ -73,6 +73,52 @@ public class Sistema {
 				}
 			}
 		});
+		
+//		Manejador m = Manejador.getInstancia(false, false);
+//		ModeloMedico me = ModeloMedico.getInstancia(false, false);
+//		
+//		Medico medico = new Medico();
+//		medico.setApellido("Ravelo");
+//		medico.setCedula("8926648484");
+//		medico.setDireccion("casas");
+//		medico.setNombre("Criseida");
+//		medico.setTelefonoCasa("809-246-0124");
+//		medico.setTelefonoCelular("809-622-4758");
+//		
+//		
+//		m.getSession().beginTransaction().begin();
+//		
+//		Usuario usuario = new Usuario("yoo","asdfa");
+//		medico.setUsuario(usuario);
+//		m.getSession().save(usuario);
+//		
+//		Especialidad es = new Especialidad();
+//		es.setNombreEspecialidad("Yo me llamo");
+//		m.getSession().save(es);
+//		Especialidad es1 = new Especialidad();
+//		es.setNombreEspecialidad("as llamo");
+//		m.getSession().save(es1);
+//		
+//		List<Especialidad> especialidades = new ArrayList<>();
+//		especialidades.add(es);
+//		especialidades.add(es1);
+//		
+//		List<RelacionEspecialidadMedico> relacion = new ArrayList<>();
+//		
+//		for (Especialidad especialidad : especialidades) {
+//			
+//			RelacionEspecialidadMedico relacionMedico = new RelacionEspecialidadMedico();
+//
+//			relacionMedico.setIdEspecialidad(especialidad);
+//			relacionMedico.setIdMedico(medico);
+//			relacion.add(relacionMedico);
+//
+//			m.getSession().save(relacionMedico);
+//
+//		}
+//		medico.setEspecialidades(relacion);
+//		m.getSession().save(medico);
+//		
+//		m.getSession().getTransaction().commit();		
 	}
-
 }
